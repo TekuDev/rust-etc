4872d7fa09aeedf893b46481fa038a6f09754b02
Teku Dev <teku.developer@gmail.com>
2022-10-24 23:13:55 +0200

    odd/even Number struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-fn main() {
-    let a: i32 = 2;
-    let b: i32 = 3;
+struct Number {
+    odd: bool,
+    value: i32
+}
 
-    println!("sum: {}", a + b);
+
+fn main() {
+    let one = Number { odd: true, value: 1 };
+    let two = Number { odd: false, value: 2 };
+
+    print_number(one);
+    print_number(two);
+    println!("#####");
+
+    for i in 0..10 {
+        let num = Number{odd: i%2 == 0, value: i};
+        print_number(num);
+    }
+}
+
+fn print_number(n: Number) {
+    match n {
+        Number {odd: true, value} => println!("Odd number: {}", value),
+        Number {odd: false, value} => println!("Even number: {}", value)
+    }
 }
